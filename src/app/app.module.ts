40ea21f371381ff6b220c1e5a5c9d65b2a12b042
Maew nohope <kanyabhak.k@ku.th>
2019-02-17 10:53:41 +0700

    shit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormComponent } from './form/form.component';
+import { DetailComponent } from './detail/detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    DetailComponent
   ],
   imports: [
     BrowserModule,
